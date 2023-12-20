33d67acb9a0cb33196e42a5262f1d90e8badd278
Pranav <pranav.techiegeek@gmail.com>
2023-12-20 17:17:19 +0530

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,28 @@ const url = "https://course-api.com/react-tours-project";
 
 function App() {
 	const [tours, setTours] = useState([]);
+	const [loading, setLoading] = useState(true);
 
 	async function fetchTours() {
-		// write code to fetch tours here
+		// set loading to true here
+
+		const data = await fetch(url);
+		const jsonData = await data.json();
+		setTours(jsonData);
+
+		// set loading to false here
+	}
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
 	}
 
 	return tours.map((tour) => {
